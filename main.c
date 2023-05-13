402e8e7290294017d208676f26e3f6b7af18e674
Aitokizu <tsareva.na2k@gmail.com>
2023-05-13 05:01:29 +0300

    update

--- main.c
+++ main.c
@@ -3,34 +3,36 @@
 #include "calc.h"
 
 void position(char *str1, char *str2) {
-    char *pos = strpbrk ( str2, str1); //функция из стандарт библ которая ищет первый из str2 в str1
-    if (pos!=NULL){
-        printf("%d",strchr(str1,*pos)-str1); //выводим индекс найденного элемента
+    int pos = -1;
+    for (int i = 0; str1[i] != '\0'; ++i){
+        for (int j = 0; str2[j] != '\0'; ++j) {
+            if (str1[i] == str2[j])
+                    pos = i;
+                    break;
+        }
     }
-    else{
-        printf("%d",-1);
-    }
+    printf("position : %d", pos);
 
 }
 
 int main() {
-/*    char str1[100], str2[100];
+   char str1[100], str2[100];
     printf("Input string 1:\n");
     scanf("%s", str1);
     printf("Input string 2:\n");
     scanf("%s", str2);
 
     position(str1,str2);
-*/
+
 
 //---------------------------------------------------------------------------------------------------
 
-    char expression[MAX_SIZE];
+    /*char expression[MAX_SIZE];
     printf("Enter an expression (promt: use s for sin and c for cos) : \n");
     fgets(expression, MAX_SIZE, stdin); // cчитываем строку с выражением
 
     float result = evaluate_expression(expression);
-    printf("Result: %f\n", result);
+    printf("Result: %f\n", result); */
 
 
     return 0;
